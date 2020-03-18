4f4849544a9a62ab819e61792f0528c8fddf5c3a
Salman <50276554+MrHooRa@users.noreply.github.com>
2020-03-18 17:43:39 +0300

    Update lab05.java

--- lab05.java
+++ lab05.java
@@ -115,7 +115,6 @@ public class gamingCenter {
 		String option;
 		int id = 0;
 		double discount = 0, price = 0;
-		Scanner input = new Scanner(System.in);
 					
 		//---------------------------------------\\
 		// Print welcome message and ask the user to enter the option.
@@ -211,38 +210,38 @@ public class gamingCenter {
 		// If the variable start = 1 ( First time ) , do ....
 		if(start == 1) {
 			System.out.print("Welcome to Gaming Center :D\n");
-			
+
 		}
-		
-		// Ask the user to enter game type.
+
+		// Ask user to enter game type.
 		System.out.print("Please, enter the type of the game: ");
 		type = input.next();
-		
-		// Ask the user to enter price of the game.
+
+		// Ask user to enter price of the game.
 		System.out.print("Please, enter the price of a game: ");
 		price = input.nextDouble();
-	
-		// Ask the user to enter number of copies of game.
+
+		// Ask user to enter number of copies of game.
 		System.out.print("Please, enter number of copies: ");
 		copies = input.nextInt();
 
-		// If the user entered game type g.
+		// If user entered game type g.
 		if(type.equals("g")) {
-			total = (price * copies) + ((price * copies) * 0.40);
-			
-			// If the copies are more than 2.
+			total = (price * (copies - 1)) * 1.40;
+
+			// If copies are more than 2.
 			if(copies > 2) {
-				total = total - ((price * copies) * 0.20); 
+				total = total - (total * 0.20) + (price * 1.40);
 
 			}
 
-		// Else , if the user entered game type n.
+		// Else , if user entered game type n.
 		}else if(type.equals("n")) {
-			total = price * copies;
-			
-			// If the user entered copies more than 3.
+			total = price * (copies - 2);
+
+			// If user entered copies more than 3.
 			if(copies > 3) {
-				total = total - ((price * copies) * 0.10); 
+				total = total - (total * 0.10) + ( price * 2 ); 
 
 			}
 
@@ -250,23 +249,22 @@ public class gamingCenter {
 		}else {
 			System.out.print("Error");
 			GameStore4(0);
-			
+
 		}
 
-		// Print Total and ask the user if want to continue or not.
+		// Print Total and ask user if want to continue or not.
 		System.out.print("Total price is: " + total);
 		System.out.print("\nDo you want to continue? yes or no: ");
 		stop = input.next();
-		
+
 		// If no, exit.
 		if(stop.equals("no")) {
-			System.out.print("Thanks, GoodBey! :D");
 			System.exit(0);
-			
+
 		// If continue, call class GameStore with variable start = 0.
 		}else if(stop.equals("yes")) {
 			GameStore4(0);
-			
+
 		}
 
 	}
